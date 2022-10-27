bfc79e5bd05b9925ee2f239f3b20d7088a82a20f
JinHui Liu <tun17225@temple.edu>
2022-10-26 21:10:00 -0400

    Finished movement animation
    Fixed run to the right problem

--- Dungeons and Dragons/Assets/Scripts/Player.cs
+++ Dungeons and Dragons/Assets/Scripts/Player.cs
@@ -13,6 +13,12 @@ public class Player : MonoBehaviour
     public SpriteRenderer sr;
     public Text PlayerNameText;
 
+    //attack
+    public GameObject attackArea;
+    private bool attacking = false;
+    private float timeToAttack = 0.25f;
+    private float timer = 0f;
+
     private Vector2 moveDirection;
 
     public float moveSpeed;
@@ -21,6 +27,7 @@ public class Player : MonoBehaviour
         if (photonView.IsMine){
             PlayerCamera.SetActive(true);
             PlayerNameText.text = PhotonNetwork.NickName;
+            
         } else {
             PlayerNameText.text = photonView.Owner.NickName;
             PlayerNameText.color = Color.cyan;
@@ -40,35 +47,66 @@ public class Player : MonoBehaviour
 
     private void checkInput()
     {
+        //move direction
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        //attack
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            attacking = true;
+            Attack(attacking);
+        }
+
+        // Attack status is true then will call the attackArea class to start attacking
+        if (attacking)
+        {
+            timer += Time.deltaTime * 2;
+
+            if (timer >= timeToAttack)
+            {
+                timer = 0;
+                attacking = false;
+                Attack(attacking);
+            }
+        }
+
         //set inputs for animatior 
         anim.SetFloat("Horizontal", moveDirection.x);
         anim.SetFloat("Vertical", moveDirection.y);
         anim.SetFloat("Speed", moveDirection.sqrMagnitude);
 
     }
+
+    public void Attack(bool attacking)
+    {
+        attackArea.SetActive(attacking);
+    }
+
     public void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-
+/*
         if (moveDirection.x < 0)
         {
-                photonView.RPC("FlipFalse", RpcTarget.AllBuffered);
+                //photonView.RPC("FlipFalse", RpcTarget.AllBuffered);
                 //FlipFalse();
         }
         if (moveDirection.x > 0)
         {
-                photonView.RPC("FlipTrue", RpcTarget.AllBuffered);
+                //photonView.RPC("FlipTrue", RpcTarget.AllBuffered);
                 //FlipTrue();
         }
-
+*/
         
         //checkFlipping();
         Debug.Log("here");
     }
+
+
+
 
     [PunRPC]
     void FlipFalse()
