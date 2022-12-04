e57eb41e9f440ac9d0314a001d469e2cb4fb3dc3
JinHui Liu <tun17225@temple.edu>
2022-12-03 23:57:56 -0500

    fix character collider
    Will not change skin if two players hit each other

--- Dungeons and Dragons/Assets/Scripts/PlayerFolder/AttackArea.cs
+++ Dungeons and Dragons/Assets/Scripts/PlayerFolder/AttackArea.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// AttackArea handler
@@ -22,19 +23,31 @@ public class AttackArea : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider)
     {
         // If the target has EnemyHealth compnent then is triggered
-        if (collider.GetComponent<Health>() != null)
+        if (collider.GetComponent<Health>() != null &&  SceneManager.GetActiveScene().name == "BattleMap")
         {
             Health health = collider.GetComponent<Health>();
+            if (collider.GetComponent<SpriteRenderer>().sprite.name == "skinSelection_2")
+            {
+                damage = 2;
+            }
             health.Damage(damage);
         }
         else if (collider.GetComponent<BOSSHealth>() != null)
         {
             BOSSHealth health = collider.GetComponent<BOSSHealth>();
+            if (collider.GetComponent<SpriteRenderer>().sprite.name == "skinSelection_2")
+            {
+                damage = 2;
+            }
             health.Damage(damage);
         }
         else if (collider.GetComponent<EnemyHealth>() != null)
         {
             EnemyHealth health = collider.GetComponent<EnemyHealth>();
+            if (collider.GetComponent<SpriteRenderer>().sprite.name == "skinSelection_2")
+            {
+                damage = 2;
+            }
             health.Damage(damage);
         }
 
