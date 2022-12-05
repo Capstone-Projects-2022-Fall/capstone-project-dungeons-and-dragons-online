b67ecb009f41a5c8c1287281db0af8a3382584b0
Yanfei Li <89934772+YanfeiLi87@users.noreply.github.com>
2022-12-05 11:46:16 -0500

    Yanfei-bring-back-files-from-PR95

---
+++ Dungeons and Dragons/Assets/Enemies/EnemySpawnPoint.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class EnemySpawnPoint : MonoBehaviour
+{
+    public GameObject enemy;
+    private float xPos;
+    private float yPos;
+    private int enemyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemis());
+    }
+
+    // Update is called once per frame
+    IEnumerator SpawnEnemis()
+    {
+        while(enemyCount < 2)
+        {
+            xPos = (Random.Range(-2, 2)/5f);
+            yPos = Random.Range(-2, 2)/5f;
+            PhotonNetwork.Instantiate(enemy.name, new Vector2(this.transform.position.x + xPos, this.transform.position.y + yPos), Quaternion.identity);
+            yield return new WaitForSeconds(80f);
+            enemyCount += 1;
+        }
+    }
+}
